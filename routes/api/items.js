e4807be0e4fa184585014499c5f67ea30abd2825
Daffa Alfandy <daffa.alfandy2@gmail.com>
2020-12-24 16:01:07 +0700

    Add update and delete for items

--- routes/api/items.js
+++ routes/api/items.js
@@ -38,4 +38,49 @@ router.get("/", async (req, res) => {
   }
 });
 
+// @route   PUT api/items/{id}
+// @desc    Update item
+// @access  public
+router.put("/:id", async (req, res) => {
+  const _id = req.params.id;
+  const { category, price } = req.body;
+
+  try {
+    let item = await Items.findById(_id);
+    if (!item) {
+      return res.status(400);
+    }
+
+    item.category = category;
+    item.price = price;
+    await item.save();
+
+    return res.json({ item });
+  } catch (err) {
+    console.log(err.message);
+    req.status(500).send("Server error");
+  }
+});
+
+// @route   DELETE api/items/{id}
+// @desc    Delete item
+// @access  public
+router.delete("/:id", async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    let item = await Items.findById(_id);
+    if (!item) {
+      return res.status(400);
+    }
+
+    await Items.deleteOne({ _id });
+
+    return res.json({ msg: "success" });
+  } catch (err) {
+    console.log(err.message);
+    req.status(500).send("Server error");
+  }
+});
+
 module.exports = router;
